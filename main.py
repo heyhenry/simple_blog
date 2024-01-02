faa9e30e44eaa7a16ed79813c349ac699002af7b
heyhenry <henryyy3@hotmail.com>
2024-01-02 14:26:26 +0800

    hello user code implemented

--- main.py
+++ main.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def index():
     return "Congratulations, it's a web app!"
 
+@app.route("/<username>")
+def hello_user(username):
+    # says hello to the given user
+    try:
+        return f"Hello, {username}"
+    except ValueError:
+        return "invalid input"
+
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
